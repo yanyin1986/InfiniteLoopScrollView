5a0f0e8746b4d083ca43b21aadfefdfb8ce06693
Leon.yan <yanyin1986@gmail.com>
2018-12-07 12:52:33 +0900

    Initial Commit

---
+++ InfiniteLoopScrollView/InfiniteLoopScrollView.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteLoopScrollView.h
+//  InfiniteLoopScrollView
+//
+//  Created by yin.yan on 2018/12/07.
+//  Copyright © 2018 Leon.yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfiniteLoopScrollView.
+FOUNDATION_EXPORT double InfiniteLoopScrollViewVersionNumber;
+
+//! Project version string for InfiniteLoopScrollView.
+FOUNDATION_EXPORT const unsigned char InfiniteLoopScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteLoopScrollView/PublicHeader.h>
+
+
